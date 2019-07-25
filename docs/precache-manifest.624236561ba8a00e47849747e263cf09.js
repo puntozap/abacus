43ab392dd8dd9d23dbff9348ae152798326b721d
puntozap <puntozap@gmail.com>
2019-07-25 12:52:08 -0400

    loading 1st test in abacus production

---
+++ docs/precache-manifest.624236561ba8a00e47849747e263cf09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fea3bcf3f579a0c4db850a0815baba",
+    "url": "/abacus/index.html"
+  },
+  {
+    "revision": "98b4b5f31092e4832f03",
+    "url": "/abacus/static/css/2.e584dfc3.chunk.css"
+  },
+  {
+    "revision": "e66f2936bd53443bdfcc",
+    "url": "/abacus/static/css/main.86629ade.chunk.css"
+  },
+  {
+    "revision": "98b4b5f31092e4832f03",
+    "url": "/abacus/static/js/2.2f5345b5.chunk.js"
+  },
+  {
+    "revision": "e66f2936bd53443bdfcc",
+    "url": "/abacus/static/js/main.015ee04e.chunk.js"
+  },
+  {
+    "revision": "04b3d69cfb9fa0ad0f1c",
+    "url": "/abacus/static/js/runtime~main.be3c5141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abacus/static/media/logo.5d5d9eef.svg"
+  }
+]);
